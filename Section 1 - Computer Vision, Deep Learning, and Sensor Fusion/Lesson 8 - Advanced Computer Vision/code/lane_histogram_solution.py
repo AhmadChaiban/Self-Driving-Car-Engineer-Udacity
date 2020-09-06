f3b77c05527e89e7dfa1d0c8d84854a7ff7a798c
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-09-06 21:43:28 +0300

    Section 1 - Lesson 8 Completed

---
+++ Section 1 - Computer Vision, Deep Learning, and Sensor Fusion/Lesson 8 - Advanced Computer Vision/code/lane_histogram_solution.py
@@ -0,0 +1,10 @@
+def hist(img):
+    # Grab only the bottom half of the image
+    # Lane lines are likely to be mostly vertical nearest to the car
+    bottom_half = img[img.shape[0]//2:,:]
+
+    # Sum across image pixels vertically - make sure to set an `axis`
+    # i.e. the highest areas of vertical lines should be larger values
+    histogram = np.sum(bottom_half, axis=0)
+    
+    return histogram
