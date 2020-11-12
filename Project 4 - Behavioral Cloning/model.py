52d294b34b0fde542a3b926db590a6b6007e7bfe
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-11-12 03:30:18 +0300

    Project 4 - Feat (nvidia architecture)

--- Project 4 - Behavioral Cloning/model.py
+++ Project 4 - Behavioral Cloning/model.py
@@ -1,33 +1,58 @@
 import tensorflow as tf
 from sklearn.metrics import r2_score
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Lambda, Cropping2D, Conv2D, MaxPooling2D, Flatten, \
+                                    Dense, Dropout, LayerNormalization
 
 class Model:
     def __init__(self):
         self.n_classes = 1
-        self.LeNet = tf.keras.Sequential([
-            tf.keras.layers.Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)),
+        self.LeNet = Sequential([
+            Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)),
+            Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)),
 
-            tf.keras.layers.Cropping2D(cropping=((50, 20), (0, 0))),
+            Conv2D(filters=24, kernel_size=(5, 5), activation='relu', strides=(1, 1), padding='valid'),
+            MaxPooling2D((2, 2)),
 
-            tf.keras.layers.Conv2D(filters=24, kernel_size=(5, 5), activation='relu', strides=(1, 1), padding='valid'),
-            tf.keras.layers.MaxPooling2D((2, 2)),
+            Conv2D(filters=36, kernel_size=(5, 5), activation='relu'),
+            MaxPooling2D((2, 2)),
 
-            tf.keras.layers.Conv2D(filters=36, kernel_size=(5, 5), activation='relu'),
-            tf.keras.layers.MaxPooling2D((2, 2)),
+            Conv2D(filters=48, kernel_size=(5, 5), activation='relu'),
+            MaxPooling2D((2, 2)),
 
-            tf.keras.layers.Conv2D(filters=48, kernel_size=(5, 5), activation='relu'),
-            tf.keras.layers.MaxPooling2D((2, 2)),
+            Conv2D(filters=64, kernel_size=(1, 1), activation='relu'),
+            MaxPooling2D((2, 2)),
 
-            tf.keras.layers.Conv2D(filters=64, kernel_size=(1, 1), activation='relu'),
-            tf.keras.layers.MaxPooling2D((2, 2)),
+            Conv2D(filters=64, kernel_size=(1, 1), activation='relu'),
+            MaxPooling2D((2, 2)),
 
-            tf.keras.layers.Conv2D(filters=64, kernel_size=(1, 1), activation='relu'),
-            tf.keras.layers.MaxPooling2D((2, 2)),
+            Flatten(),
 
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(120, activation='relu', kernel_regularizer='l2'),
-            tf.keras.layers.Dense(84, activation='relu'),
-            tf.keras.layers.Dense(self.n_classes, activation='sigmoid')
+            Dense(120, activation='relu', kernel_regularizer='l2'),
+            Dense(84, activation='relu'),
+            Dense(self.n_classes, activation='sigmoid')
+        ])
+
+        self.nvidia = Sequential([
+            Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)),
+            Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)),
+
+            LayerNormalization(epsilon=0.001),
+
+            Conv2D(filters=24, kernel_size=(5, 5), activation='relu', strides=(1, 1), padding='valid'),
+            Conv2D(filters=36, kernel_size=(5, 5), activation='relu'),
+            Conv2D(filters=48, kernel_size=(5, 5), activation='relu'),
+            Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+            Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+
+            Flatten(),
+
+            Dense(1164, activation='relu'),
+            Dense(100, activation='relu'),
+            Dense(50, activation='relu'),
+            Dense(10, activation='relu'),
+            Dense(self.n_classes, activation='sigmoid')
+
         ])
 
         self.inception = self.prepare_inception()
@@ -40,38 +65,38 @@ class Model:
         for layer in inception_model.layers:
             layer.trainable = False
         # Flatten the output layer to 1 dimension
-        x = tf.keras.layers.Flatten()(inception_model.output)
+        x = Flatten()(inception_model.output)
         # Add a fully connected layer with 1,024 hidden units and ReLU activation
-        x = tf.keras.layers.Dense(1024, activation='relu')(x)
+        x = Dense(1024, activation='relu')(x)
         # Add a dropout rate of 0.2
-        x = tf.keras.layers.Dropout(0.2)(x)
+        x = Dropout(0.2)(x)
         # Add a final sigmoid layer for classification
-        x = tf.keras.layers.Dense(self.n_classes, activation='sigmoid')(x)
+        x = Dense(self.n_classes, activation='sigmoid')(x)
         return tf.keras.Model(inception_model.input, x)
 
     def train(self, X_train, y_train):
         epochs = 5
-        batch_size = 32
+        batch_size = 512
         optimizer = tf.keras.optimizers.SGD(learning_rate=0.1, name='SGD')
         loss_function = tf.keras.losses.log_cosh
-        self.LeNet.compile(optimizer=optimizer, loss=loss_function, metrics=['mae'])
+        self.nvidia.compile(optimizer=optimizer, loss=loss_function, metrics=['mae'])
         history = []
-        for i in range(0, 3):
-            history.append(self.LeNet.fit(x=X_train[:, i],
-                                          y=y_train,
-                                          batch_size = batch_size,
-                                          epochs=epochs,
-                                          validation_split=0.2)
+        for i in range(0, 1):
+            history.append(self.nvidia.fit(x=X_train[:, i],
+                                           y=y_train,
+                                           batch_size=batch_size,
+                                           epochs=epochs,
+                                           validation_split=0.2)
                            )
         return history
 
     def test(self, X_test, y_test):
         scores=[]
-        for i in range(0, 3):
-            y_pred = self.LeNet.predict(X_test[:, i])
+        for i in range(0, 1):
+            y_pred = self.nvidia.predict(X_test[:, i])
             scores.append(r2_score(y_test, y_pred))
         print(scores)
         return scores
 
     def save(self, path):
-        self.LeNet.save(path)
+        self.nvidia.save(path)
